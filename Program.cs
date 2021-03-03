c3b30ce996f294712fa0f75df5e4724bc8bdb1e7
Yaritza <yaritzamicaelleb@gmail.com>
2021-03-03 08:27:12 -0300

    Contagem Regressiva

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Atividade2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep();
+            Console.ForegroundColor= ConsoleColor.DarkRed;
+            Console.WriteLine("Pressione uma tecla para iniciar a contagem");
+            Console.ResetColor();
+            Console.ReadKey();
+            Console.Beep();
+            Console.WriteLine("Preparar...");
+            Console.Beep();
+            Console.ReadKey();
+            Console.WriteLine("Apontar...");
+            Console.Beep();
+            Console.ReadKey();
+            Console.WriteLine("Fogo!");
+            Console.Beep();
+        }
+    }
+}
